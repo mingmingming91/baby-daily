740273d3dc6ec3e0a5d635b3fc74f10f39fb44b8
mingmingming91 <shu0913@gmail.com>
2026-04-16 16:00:39 +0800

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'baby-daily-v2';
+const CACHE_NAME = 'baby-daily-v3';
 self.addEventListener('install', e => {
     e.waitUntil(
         caches.open(CACHE_NAME).then(cache => cache.addAll(['index.html']))
