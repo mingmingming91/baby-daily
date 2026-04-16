fd41b0456500397a32dffab8681acc942d1d166e
mingmingming91 <shu0913@gmail.com>
2026-04-16 10:51:06 +0800

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,18 +1,11 @@
-const CACHE_NAME = 'baby-daily-v1';
-const ASSETS = [
-  'index.html',
-  'https://unpkg.com/vue@3/dist/vue.global.js',
-  'https://cdn.tailwindcss.com'
-];
-
-self.addEventListener('install', (e) => {
-  e.waitUntil(
-    caches.open(CACHE_NAME).then((cache) => cache.addAll(ASSETS))
-  );
+const CACHE_NAME = 'baby-daily-v2';
+self.addEventListener('install', e => {
+    e.waitUntil(
+        caches.open(CACHE_NAME).then(cache => cache.addAll(['index.html']))
+    );
 });
-
-self.addEventListener('fetch', (e) => {
-  e.respondWith(
-    caches.match(e.request).then((res) => res || fetch(e.request))
-  );
+self.addEventListener('fetch', e => {
+    e.respondWith(
+        caches.match(e.request).then(res => res || fetch(e.request))
+    );
 });
